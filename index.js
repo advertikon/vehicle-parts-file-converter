49a6006bd875a8225ae05613fdd76c6e5b21b915
Maxim Bogochas <bogochunas@gmail.com>
2022-10-30 13:22:03 +0200

    feat: release

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const program = new Command();
 program
     .command('convert <file-name>')
     .description('Convert file to proper format')
-    .option('-p --prefix <prefix>', 'output file prefix')
+    .requiredOption('-n --namespace <namespace>', 'DB namespace')
     .option('--separator-in <delimiter-in>', 'input file fields delimiter', ',')
     .option('--separator-out <delimiter-out>', 'output file fields delimiter', '|')
-    .action(async (fileName, options) => {console.log(options)
-        const { delimiterIn, delimiterOut } = options;
-        await Converter(fileName, delimiterIn, delimiterOut).catch(e => console.error(`Error: ${e.message}`.red));
+    .action(async (fileName, options) => {
+        const { delimiterIn, delimiterOut, namespace } = options;
+        await Converter(fileName, namespace, delimiterIn, delimiterOut).catch(e => console.error(`Error: ${e.message}`.red));
     });
 
 await program.parseAsync();
