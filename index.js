da3590059f64d56dc0d2efb10b49782bc10bf11c
Maxim Bogochunas <bogochunas@gmail.com>
2023-05-18 20:48:09 +0300

    feat: add output directory

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { Command } from 'commander';
 import colors from 'colors';
 import { Converter } from './lib/converter.js';
+import fs from 'fs';
 
 const program = new Command();
+program.version(JSON.parse(fs.readFileSync('package.json')).version, '-v, --version')
 
 program
     .command('convert <file-name>')
@@ -10,9 +12,10 @@ program
     .requiredOption('-n --namespace <namespace>', 'table name')
     .option('--separator-in <delimiter-in>', 'input file fields delimiter', ',')
     .option('--separator-out <delimiter-out>', 'output file fields delimiter', '|')
+    .option('--out-dir <output-dir>', 'output directory', '.')
     .action(async (fileName, options) => {
-        const { delimiterIn, delimiterOut, namespace } = options;
-        await Converter(fileName, namespace, delimiterIn, delimiterOut).catch(e => console.error(`Error: ${e.message}`.red));
+        const { separatorIn, separatorOut, namespace, outDir } = options;
+        await Converter(fileName, namespace, separatorIn, separatorOut, outDir).catch(e => console.error(`Error: ${e.message}`.red));
     });
 
 await program.parseAsync();
